f2b14ed808dcd32d159e41a5fb1be8731eae215d
aarohi <prathmeshgaikwad0302@gmail.com>
2021-01-11 11:19:48 +0530

    assignment

---
+++ Test/src/com/zensar/test/LoginRepository.java
@@ -0,0 +1,26 @@
+package com.zensar.test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.utility.DBUtil;
+
+public class LoginRepository {
+	public String Login(String username, String password) throws SQLException {
+		Connection	con = DBUtil.getCon();
+		String sql = "select * from login where name=? and password=?";
+		ResultSet rs = null;
+		try {
+			PreparedStatement pst = con.prepareStatement(sql);
+			pst.setString(1, username);
+			pst.setInt(2, Integer.parseInt(password));
+			rs = pst.executeQuery();
+		} catch (SQLException e) {
+			System.out.println("Error from Login Repository: "+e);
+			e.printStackTrace();
+		}
+		return ""+rs.next()+":"+username;
+//		return rs.next();
+	}
+}
